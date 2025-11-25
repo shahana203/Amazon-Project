a24373f0aed01906091222c1dd230efce3c7fe58
shahana203 <shahana2003j@gmil.com>
2025-11-25 12:19:56 +0530

    frontend url removed

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(cors({
     origin: [
         "http://localhost:5173",  
-        "https://amazon-project-murex.vercel.app" 
+        
     ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
