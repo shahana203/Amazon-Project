19f003cac6f795e8e8c1409902cccbfb76faa7ca
shahana203 <shahana2003j@gmil.com>
2025-11-25 09:54:43 +0530

    changes added in index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,17 @@ require('dotenv').config();
 require('./controllers/googleAuth');
 
 const app = express();
-app.use(cors());
+app.use(cors({origin: [
+        "http://localhost:5173",
+        
+      
+    ],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, 
+    optionsSuccessStatus: 200
+}));
+
+
 app.use(express.json());
 app.use(express.static('public'));
 
