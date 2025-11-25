bdd99fda857a4cf530fa6fd291f7a7157ad39df3
shahana203 <shahana2003j@gmil.com>
2025-11-25 12:45:38 +0530

    frontend url added

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(cors({
     origin: [
         "http://localhost:5173",  
-        
+        "https://amazon-project-frontend.vercel.app"
     ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
